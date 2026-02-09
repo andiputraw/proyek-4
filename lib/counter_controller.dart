07b0b61673171a53fe9509f34e2c4110e57bfa8b
andiputraw <andiputraw925@gmail.com>
2026-02-09 10:12:13 +0700

    added button for kurang and reset, added history

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -1,19 +1,40 @@
+import 'dart:collection';
+
+import 'package:intl/intl.dart';
+
 class CounterController {
   int step = 1;
   int _counter = 0;
   int get value => _counter;
+  final Queue<String> _history = Queue();
+  List<String> get history => _history.toList();
 
   void increment() {
     _counter += step;
+    pushHistory("+$step : ${now()}");
   }
 
   void decrement() {
     if (_counter > 0) {
       _counter -= step;
+      pushHistory("-$step : ${now()}");
     }
   }
 
   void reset() {
     _counter = 0;
+    pushHistory("reset : ${now()}");
+  }
+
+  void pushHistory(String str) {
+    if (_history.length >= 5) {
+      _history.removeFirst();
+    }
+    _history.addLast(str);
+  }
+
+  String now() {
+    DateTime now = DateTime.now();
+    return DateFormat('yyyy-mm-dd hh:mm').format(now);
   }
 }
--- lib/counter_view.dart
+++ lib/counter_view.dart
@@ -18,29 +18,56 @@ class _CounterViewState extends State<CounterView> {
       body: Center(
         child: Column(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            const Text("Total hitungan"),
+            Text("${_controller.value}", style: const TextStyle(fontSize: 40)),
+            Row(
               children: [
-                const Text("Total hitungan"),
-                Text(
-                  "${_controller.value}",
-                  style: const TextStyle(fontSize: 40),
-                ),
-                Row(
-                  children: [
-                    Text("Step: ${_controller.step}"),
-                    Expanded(
-                      child: Slider(
-                        min: 0,
-                        max: 100,
-                        value: _controller.step.toDouble(),
-                        onChanged: (v) =>
-                            setState(() => _controller.step = v.toInt()),
-                      ),
-                    ),
-                  ],
+                Text("Step: ${_controller.step}"),
+                Expanded(
+                  child: Slider(
+                    min: 0,
+                    max: 100,
+                    value: _controller.step.toDouble(),
+                    onChanged: (v) =>
+                        setState(() => _controller.step = v.toInt()),
+                  ),
                 ),
               ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () => setState(() => _controller.increment()),
+                  style: ButtonStyle(),
+                  child: Text("Tambah"),
+                ),
+                OutlinedButton(
+                  onPressed: () => setState(() => _controller.decrement()),
+                  style: ButtonStyle(),
+                  child: Text("Kurang"),
+                ),
+                OutlinedButton(
+                  onPressed: () => setState(() => _controller.reset()),
+                  style: ButtonStyle(),
+                  child: Text("Reset"),
+                ),
+              ],
+            ),
+            const Text(
+              "History",
+              style: TextStyle(fontSize: 42, fontWeight: FontWeight.w700),
+            ),
+            const Divider(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _controller.history.length > 5
+                    ? 5
+                    : _controller.history.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Text(_controller.history[index]);
+                },
+              ),
             ),
           ],
         ),
